cce687e47b5009815bedeae076a58ab3c075af3c
YegoEDDY <35481971+YegoEDDY@users.noreply.github.com>
2024-12-08 10:31:32 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -23,16 +23,16 @@ Qᴜᴇᴇɴ ᴇʟɪꜱᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ`,
   PREFIX: '.$&',
   LANGUAGE: 'EN',
   PORT: 3000,
-  BOT_NAME: '𝐐𝐔𝐄𝐄𝐍 𝐄𝐋𝐈𝐒𝐀',
-  OWNER_NAME: 'ᴍ ʀ  ɴ ɪ ᴍ ᴀ',
-  OWNER_NUMBER: '94715166712',
-  SUDO: '94715166712',
+  BOT_NAME: 'YEGO',
+  OWNER_NAME: 'YEGO',
+  OWNER_NUMBER: '254705677688',
+  SUDO: '254705677688',
   WORK_TYPE: 'public',
   WORK_MODE: 'group',
   TIME_ZONE: 'Asia/Colombo',
-  CAPTION: '© ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ Qᴜᴇᴇɴ ᴇʟɪꜱᴀ ᴍᴅ',
-  STICKER_AUTHOR: 'Qᴜᴇᴇɴ ᴇʟɪꜱᴀ',
-  STICKER_PACK: 'ᴍʀ ɴɪᴍᴀ ᴏꜰᴄ',
+  CAPTION: '© YEGOᴅ',
+  STICKER_AUTHOR: 'YEGO',
+  STICKER_PACK: 'YEGO',
   REMOVE_BG_APIKEY: '',
   MAXIMUM_FILESIZE: 90,
   OWNER_R_EMOJI: '👽',
@@ -40,7 +40,7 @@ Qᴜᴇᴇɴ ᴇʟɪꜱᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ`,
   AUTO_VOICE: false,
   INBOX_BLOCK: false,
   OWNER_REACT: true,
-  AUTO_REACT: true,
+  AUTO_REACT: false,
   ONLY_PM_GROUP_MSG_SEND: false,
   AUTO_SEEN_STATUS: true,
   BOT_OFFLINE: false,
@@ -61,16 +61,16 @@ Qᴜᴇᴇɴ ᴇʟɪꜱᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ`,
   FB_DOWNLOAD_TYPE: 'selectable', // hdvideo , sdvideo
   TIKTOK_DOWNLOAD_TYPE: 'selectable', // nowmvideo , wmvideo
   MSG: {
-    ONLY_GROUP_MSG: 'Bot only Can Use Groups!',
-    ONLY_PM_MSG: 'Bot only Can Use Private Chats!',
-    BAD_DETECT_MSG: '@user Bad Words Are Disable by Owner!',
-    BANED_MSG: "@user You Can't use bot, You are baned by owner!",
-    GROUP_LEAVE: '👋 Bye @groupName I am Going.',
+    ONLY_GROUP_MSG: '',
+    ONLY_PM_MSG: '',
+    BAD_DETECT_MSG: '',
+    BANED_MSG: "",
+    GROUP_LEAVE: '',
     INBOX_BLOCK:
-      '❌ My owner has activated auto inbox blocking. So you will be auto blocked after 8 seconds'
+      ''
   },
   BOT_RULES:
-    "*⚖️ 𝚀𝚄𝙴𝙴𝙽 𝙴𝙻𝙸𝚂𝙰 𝙱𝙾𝚃 𝚁𝚄𝙻𝙴𝚂 ⚖️*\n\n☫ Don't spam messages.\n☫ Don't add groups without asking owner.\n"
+    ""
 }
 
 let file = require.resolve(__filename)
